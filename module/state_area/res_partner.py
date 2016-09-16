097d731293ca5cf33922d9b9a94b7b44fd55e2c4
cahyoagung <cahyo.inbox@gmail.com>
2016-09-16 15:29:22 +0700

    update module 'state_area'

--- module/state_area/res_partner.py
+++ module/state_area/res_partner.py
@@ -3,7 +3,15 @@ from openerp import models, fields, api
 class res_partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
-    state_area = fields.Many2one(comodel_name='state.area',delegate=True)
+    state_area = fields.Many2one(comodel_name='state.area',delegate=False,store=False,compute='state_area_compute')
+    
+    def state_area_compute(self):
+        for obj in self:
+            if obj.state_id:
+                state=obj.state_id
+                if state.state_area:
+                    area=state.state_area
+                    obj.state_area=area.id
     
     @api.multi
     def onchange_state(self, state_id):
