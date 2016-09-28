1cf9a6d1b5acf142df85ad2e0dca0666a2eb6283
cahyoagung <cahyo.inbox@gmail.com>
2016-09-28 15:49:12 +0700

    update add new

--- module/ws_helmigs/helmigs_controller.py
+++ module/ws_helmigs/helmigs_controller.py
@@ -1,5 +1,7 @@
 import openerp.http as http
 from openerp.http import request
+from openerp.http import Response
+import werkzeug
 
 class HelmigsController(http.Controller):
 
@@ -7,6 +9,12 @@ class HelmigsController(http.Controller):
     def some_html(self):
         return "<h1>This is a test</h1>"
 
-    @http.route('/my_url/some_json', type="json")
+    @http.route('/my_url/some_json', type="json", auth='none', methods=['GET','POST'], website=True)
     def some_json(self):
         return {"sample_dictionary": "This is a sample JSON dictionary"}
+
+
+class MyController(http.Controller):
+    @http.route('custom/myjson', type='json', auth='none', methods=['GET','POST'], website=True)
+    def myjson(self):
+        return {"data":"cahyo agung prastio"}
